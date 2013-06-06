95d2494ad1e1a1b607f8be8a146f7f71bdd7c7a7
James Roseman <james.roseman@gmail.com>
2013-06-06 12:48:05 -0400

    Grabs top 100 links instead of just top 25

--- wallpaper.sh
+++ wallpaper.sh
@@ -53,14 +53,14 @@ RESOLUTION=$X"x"$Y
 # Format the sublist
 python sublist_format.py 
 
-CURR=`shuf -i 0-100 -n 1`
+CURR=`shuf -i 0-250 -n 1`
 NEXT=`echo $(($CURR + 1))`
 USR=`whoami`
 HOMEDIR="/home/$USR/mystique"
 PYSCRAPE=$HOMEDIR
 SUBS=`cat $HOMEDIR/.sublist`
 PRE="/search.json?q=%5B"
-POS="%5D&restrict_sr=on&sort=relevance&t=all&limit=100"
+POS="%5D&restrict_sr=on&sort=relevance&t=all&limit=250&sort=all"
 SUBLIST=$SUBS$PRE$RESOLUTION$POS
 IMGURL=`$PYSCRAPE/img_scrape.py $SUBLIST $CURR`
 PICDIRPATH="/home/$USR/Pictures/wallpapers/"
@@ -92,3 +92,4 @@ mv $IMGNAME $PICDIRPATH
 
 gsettings set org.gnome.desktop.background picture-uri "file://$IMGPATH"
 echo $IMGURL
+echo $IMGPATH
